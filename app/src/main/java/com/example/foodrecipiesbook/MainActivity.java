37ba2925053ca648ca82a42a379f2e08f0f947d7
Ahmed Iqbal Bhatti <ahmad786687iqbal@gmail.com>
2022-08-14 02:42:01 +0500

    Splash Screen Design is Done for Now

--- app/src/main/java/com/example/foodrecipiesbook/MainActivity.java
+++ app/src/main/java/com/example/foodrecipiesbook/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.foodrecipiesbook;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +11,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        Thread thread = new Thread() {
+            public void run() {
+                try {
+                    sleep(1000);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                } finally {
+                    Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+//                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        }; thread.start();
     }
 }
