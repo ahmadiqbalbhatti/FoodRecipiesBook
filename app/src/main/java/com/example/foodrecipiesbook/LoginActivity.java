cc148286c26942c4a221b60b042d261f1759a5a9
Ahmed Iqbal Bhatti <ahmad786687iqbal@gmail.com>
2022-08-21 21:48:00 +0500

    Nearly Interface is Done

--- app/src/main/java/com/example/foodrecipiesbook/LoginActivity.java
+++ app/src/main/java/com/example/foodrecipiesbook/LoginActivity.java
@@ -3,31 +3,64 @@ package com.example.foodrecipiesbook;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
+
+    Button logInButton;
+    Button signUpButton;
+    TextView forgotPassword;
+
+    TextView emailTextView;
+    TextView passwordTextView;
+
+    Intent intent;
+
+    private void navigateTo(Context context, Class navigateToClass){
+        intent = new Intent(context, navigateToClass);
+        startActivity(intent);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        Button signUpButton = findViewById(R.id.signUpButton);
-        TextView forgotPassword = findViewById(R.id.forgotPassword);
-//        TextView createAnAccount = findViewById(R.id.createAnAccount);
+        logInButton = findViewById(R.id.logInButton);
+        signUpButton = findViewById(R.id.signUpButton);
+        forgotPassword = findViewById(R.id.forgotPassword);
+
+        emailTextView = findViewById(R.id.emailTextView);
+        passwordTextView = findViewById(R.id.passwordTextView);
+
+
+        logInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (emailTextView == null && passwordTextView == null){
+                    Toast.makeText(LoginActivity.this, "Please Enter Email and Password", Toast.LENGTH_SHORT).show();
+                }else{
+                    navigateTo(LoginActivity.this, HomeActivity.class);
+                }
+            }
+        });
+
+
 
 
 
         signUpButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent signUpIntent = new Intent(LoginActivity.this, SignUpActivity.class);
-                startActivity(signUpIntent);
-
+//                intent = new Intent(LoginActivity.this, SignUpActivity.class);
+//                startActivity(intent);
+                navigateTo(LoginActivity.this, SignUpActivity.class);
             }
         });
 
@@ -35,20 +68,11 @@ public class LoginActivity extends AppCompatActivity {
         forgotPassword.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent signUpIntent = new Intent(LoginActivity.this, ForgotPasswordActivity.class);
-                startActivity(signUpIntent);
-
+//                intent = new Intent(LoginActivity.this, ForgotPasswordActivity.class);
+//                startActivity(intent);
+                navigateTo(LoginActivity.this, SignUpActivity.class);
             }
         });
 
-
-//        createAnAccount.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                Intent signUpIntent = new Intent(LoginActivity.this, SignUpActivity.class);
-//                startActivity(signUpIntent);
-//
-//            }
-//        });
     }
 }
